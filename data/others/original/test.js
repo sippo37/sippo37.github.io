bb2445bdcf2d6d4c30f8466cd2cbb2cdfe109a0c
sippo37 <sippo37@gmail.com>
2022-08-04 09:34:50 +0900

    update20220804

--- data/others/original/test.js
+++ data/others/original/test.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function checkForm($this)
 {
     var str=$this.value;
@@ -7,14 +6,4 @@ function checkForm($this)
         str=str.replace(/[^ -~｡-ﾟ]/,"");
     }
     $this.value=str;
-=======
-function checkForm($this)
-{
-    var str=$this.value;
-    while(str.match(/[^A-Z^a-z\d\-]/))
-    {
-        str=str.replace(/[^A-Z^a-z\d\-]/,"");
-    }
-    $this.value=str;
->>>>>>> 29180a2ebe7b73f34919091b488e77f4bab21fa8
 }
